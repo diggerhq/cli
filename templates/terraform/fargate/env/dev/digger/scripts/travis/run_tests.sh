56f442607c44d5d612fdd34d10e8e65a7aa7689b
motatoes <moe.habib9@gmail.com>
2020-11-10 17:37:53 +0000

    remove terraform files from cli

--- templates/terraform/fargate/env/dev/digger/scripts/travis/run_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-flake8 . &&
-black --check . &&
-python scripts/local/wait_for_postgres.py &&
-pytest --cov=./
--- templates/terraform/fargate/env/dev/digger/scripts/travis/script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker-compose --version
-docker-compose -f docker-compose.test.yml build
-docker-compose -f docker-compose.test.yml run --rm web scripts/travis/run_tests.sh
