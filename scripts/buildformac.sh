b6e9ebaa32b3976bb017fb17cc7d8185dbf79013
motatoes <moe.habib9@gmail.com>
2020-12-22 00:16:17 +0300

    removal of workdir

--- scripts/buildformac.sh
+++ scripts/buildformac.sh
@@ -1,6 +1,5 @@
 # cd dg
 pip3 install pyinstaller
 pip3 install -r requirements.txt
-pip install .
-pyinstaller --clean -y --dist ./dist/dg-mac --workpath /tmp dg.spec
+pyinstaller --clean -y --dist ./dist/dg-mac
 chmod +x dist/dg-mac/dg
