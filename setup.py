9d49d2b9c9fd6fca57f9bdfcda50bbafef746dac
motatoes <moe.habib9@gmail.com>
2020-12-22 18:49:19 +0300

    include diggercli.utils package

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=[
         "click",
     ],
-    packages=['diggercli'],
+    packages=['diggercli', 'diggercli.utils'],
     entry_points='''
         [console_scripts]
         dg=dg.dg:cli
