4210483ea92fcfac9e973b21127acac54f74a8fa
motatoes <moe.habib9@gmail.com>
2020-12-22 19:41:55 +0300

    revert to old setup

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@ from setuptools import setup
 
 setup(
     name='Digger CLI',
-    package_dir={
-        'diggercli': ''
-    },
+    # package_dir={
+    #     'diggercli': ''
+    # },
     version="1.0",
     py_modules=["diggercli",],
     install_requires=[
         "click",
     ],
+    packages=['diggercli', 'diggercli.utils'],
     entry_points='''
         [console_scripts]
         dg=diggercli.dg:cli
