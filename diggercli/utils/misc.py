156db7122468390c8c062563d48ac7c2c1506156
motatoes <moe.habib9@gmail.com>
2021-04-13 17:04:42 +0300

    fix the split command

--- diggercli/utils/misc.py
+++ diggercli/utils/misc.py
@@ -5,7 +5,7 @@ def parse_env_config_options(config : list):
         if configOption.find("=") < 0:
             Bcolors.error(f"each config should be of form key=val, found: {configOption}")
             sys.exit(-1)
-        key,val = configOption.split("=")
+        key,val = configOption.split("=", 1)
         # parse boolean inputs correctly
         if val.lower() == "true" or val.lower() == "false":
             val = (val.lower() == "true")
